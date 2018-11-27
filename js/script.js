5dc9bd103d2b05c848683afeee61319258144d81
Charles Badger <charles.badger@gmail.com>
2018-11-26 21:50:10 -0600

    add navigation and layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+
 console.log('******************************');
 console.log('GATEWAY GAUNTLET - Coming Soon');
 console.log('******************************');
+
+$('body').scrollspy({ target: '#nav' });
