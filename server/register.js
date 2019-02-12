99ef89ef7bacb4b6e901546fe8e32ec143ed9103
Charles Badger <charles.badger@gmail.com>
2019-02-12 14:09:48 -0600

    add register.js

---
+++ server/register.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.get('/', (req, res) => {
+  res.json({
+    test: 'test',
+  });
+});
+
+app.use('/.netlify/functions/server', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
