65a3acab6199c4a990dfd4f87c91a7f9cadcd23c
Charles Badger <charles.badger@gmail.com>
2019-02-12 14:23:09 -0600

    modify register.js

--- server/register.js
+++ server/register.js
@@ -5,16 +5,20 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-router.get('/', (req, res) => {
-  res.json({
-    test: 'test',
-  });
+// router.get('/', (req, res) => {
+//   res.json({
+//     test: 'test',
+//   });
+// });
+
+// app.use('/.netlify/functions/register', router);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.use('/.netlify/functions/register', router);
-
-module.exports = app;
+// module.exports = app;
 module.exports.handler = serverless(app);
