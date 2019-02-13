781031638f9ec1053bdc08f5a197c2290bd79482
Charles Badger <charles.badger@gmail.com>
2019-02-12 20:00:41 -0600

    add cors to register.js

--- lambda/register.js
+++ lambda/register.js
@@ -9,6 +9,13 @@ app.use(bodyParser.json());
 
 const router = express.Router();
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/register', (req, res) => {
   res.json({
     test: 'message',
