8509068687615fc9357efdf7eba4a9536424dcb2
Charles Badger <charles.badger@gmail.com>
2019-02-12 20:32:16 -0600

    change register.js

--- lambda/register.js
+++ lambda/register.js
@@ -1,34 +1,41 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const serverless = require('serverless-http');
+// const bodyParser = require('body-parser');
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-const router = express.Router();
+// const router = express.Router();
 
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+// app.use((req, res, next) => {
+//   res.append('Access-Control-Allow-Origin', ['*']);
+//   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.append('Access-Control-Allow-Headers', 'Content-Type');
+//   res.append('Access-Control-Max-Age', '2592000');
+//   res.append('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
+
+// app.get('/register', (req, res) => {
+//   res.json({
+//     test: 'message',
+//   });
+// });
+
+// app.post('/register', (req, res) => {
+//   console.log(req.body);
+
+//   res.json({
+//     message: req.body.message,
+//   });
+// });
+
+// module.exports = router;
+// module.exports.handler = serverless(app);
+
+exports.handler = async (event, context) => ({
+  statusCode: 200,
+  body: 'Hello World',
 });
-
-app.get('/register', (req, res) => {
-  res.json({
-    test: 'message',
-  });
-});
-
-app.post('/register', (req, res) => {
-  console.log(req.body);
-
-  res.json({
-    message: req.body.message,
-  });
-});
-
-module.exports = router;
-module.exports.handler = serverless(app);
