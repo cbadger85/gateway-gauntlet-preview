375fc5ca7f746460b9f8ad9fe60cb439d27fd94d
Charles Badger <charles.badger@gmail.com>
2019-02-18 15:32:29 -0600

    cleanup payment.js

--- src/js/modules/payment.js
+++ src/js/modules/payment.js
@@ -37,8 +37,6 @@ const payment = (element, form) => {
 
       document.querySelector('form').style.display = 'none';
       document.getElementById('success').style.display = 'block';
-
-      // TODO: toggle modal with either success and redirect or error message.
     } catch (err) {
       submittingPayment(false);
 
@@ -74,17 +72,13 @@ const payment = (element, form) => {
 
     const { name, state, zip } = e.target.elements;
 
-    // console.log(state.value, zip.value);
-
     const { token, error } = await stripe.createToken(card, {
       name: name.value,
       address_state: state.value,
       address_zip: zip.value,
     });
 
-    if (error) {
-      // TODO: trigger modal with error
-    } else {
+    if (!error) {
       submitToken(token, e.target.elements);
     }
   });
